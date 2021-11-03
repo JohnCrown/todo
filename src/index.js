0177177005f7cfac197f9470fd0eb99d32cba0f0
JohnCrown <36449663+JohnCrown@users.noreply.github.com>
2021-11-03 20:58:59 +0200

    todo-first-app
    first-app

--- src/index.js
+++ src/index.js
@@ -1,17 +1,67 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import App from './components/app/';
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// class WhoAmI extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             years: 26
+            
+//         }
+            //Способы использовать функцию как обраб-к события
+        // this.nextYear = this.nextYear.bind(this); //1 Cпособ привязать
+        // this.nextYear = () => {  //2 Способ
+        //     this.setState(state => ({
+        //         years: ++state.years
+        //     }))
+        // }
+    // }                                                  
+        // nextYear = () => {                           
+        // this.setState(state => ({
+        //     years: ++state.years
+        // }))
+ 
+        
+
+    
+    // }
+    // state = { //ES 9 Для ClassField
+    //     years: 26
+    // }
+    // nextYear() {
+    //     this.setState(state => ({
+    //         years: ++state.years
+
+    //     }))
+    // }
+//     render() {
+//         const {name, surname, link} = this.props;
+//         const {years} = this.state;
+//         return (
+//             <>  
+//                 <button onClick={this.nextYear} >++</button>
+//                 <h1>My name is {name}, surname - {surname}, years = {years}</h1>
+//                 <a href ={link}>My profile</a>
+//             </>
+            
+//         )
+
+//     }
+// }
+
+
+// const All = () => {
+//     return (
+//         <>
+//             <WhoAmI name="John" surname="Smith" link="google.com" />
+//             <WhoAmI name="Jane" surname="Smith" link="vk.com" />
+//             <WhoAmI name="Alex" surname="Twitch" link="google.com" />
+//         </>
+//     )
+// }
+
+ReactDOM.render(<App/> ,document.getElementById('root'));
+
